df6014a2a3c5244fdc1520183205e5160c99c9a6
Alberto Morgante <alberto.morgante@orangex.es>
2018-11-03 16:30:18 +0100

    fix typo

--- config/messages.go
+++ config/messages.go
@@ -2,7 +2,7 @@ package config
 
 const (
 	SpeechOnLaunch 			= "Bienvenido al sistema de alarma de seguridad"
-	SpeechOnActivateFull 	= "Alarma de Seguridad Activada Completamente. Tienes 30 segundos de casa"
+	SpeechOnActivateFull 	= "Alarma de Seguridad Activada Completamente. Tienes 30 segundos para salir de casa"
 	SpeechOnActivatePartial = "Alarma de Seguridad Activada solo para el perímetro. Dentro de 30 segundos los detectores de presencia serán desactivados"
 	SpeechOnDeactivate 		= "Alarma de Seguridad Desactivada. Puedes entrar con seguridad en casa"
 	SpeechOnStatusONFull	= "La alarma está activada completamente"
